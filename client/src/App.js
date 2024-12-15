a4998a9bd6ca4b45a9e4dd70cb613dfc148efc65
naman-p06 <patelneeta1104@gmail.com>
2024-12-15 17:00:00 +0530

    ini

---
+++ client/src/App.js
@@ -0,0 +1,52 @@
+import logo from './logo.svg';
+import './App.css';
+import TodoForm from './TodoForm';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  useEffect(() => {
+    axios.get('http://localhost:5000/todos')
+      .then(response => setTodos(response.data))
+      .catch(error => console.error(error));
+  }, []);
+  const addTodo = (newTodo) => {
+    setTodos([...todos, newTodo]);
+  };
+
+  return (
+    <div>
+      <h1>MERN Stack Todo App</h1>
+      <TodoForm onAdd={addTodo} />
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo._id}>{todo.task}</li>
+        ))}
+      </ul>
+
+
+    </div>
+  );
+};
+export default App;
